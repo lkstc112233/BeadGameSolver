3df9e14fe431e5a7a7176af649862244da2c8b70
Kevin Xu Ke <lkstc112233@gmail.com>
2016-04-30 22:20:05 +0800

    Initial Commit

---
+++ BeadGameSolver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BeadGameSolver
+//
+//  Created by 煦 柯 on 16/4/30.
+//  Copyright © 2016年 Must Studio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
